9c80a8ad80e1d11af66a8c4b450fce17f1d7e5be
minnug <parkminnug@gmail.com>
2024-03-22 22:24:15 +0900

    ✨ 신규 프로필 페이지 (CREATE)

--- js/createProfile.js
+++ js/createProfile.js
@@ -1,7 +1,7 @@
+// 이미지 업로드
 const fileInput = document.querySelector('.upload-file .input-file');
 const deleteBtn = document.querySelector('.upload-file .btn-cancel');
 
-// 이미지 업로드
 fileInput.addEventListener('change', () => {
   const fileReader = new FileReader();
   fileReader.addEventListener('load', () => {
@@ -18,3 +18,59 @@ deleteBtn.addEventListener('click', () => {
   fileInput.value = '';
   deleteBtn.style.display = 'none';
 });
+
+// 폼 입력 제출
+const newForm = document.querySelector('#newForm');
+
+class View {
+  constructor (indexNum, nicknameStr, kindStr,traitStr) {
+    this.index = indexNum;
+    this.Nickname = nicknameStr;
+    this.Kind= kindStr;
+    this.Trait = traitStr;
+  }
+
+  set Nickname(value) {
+    if(value.length === 0) throw new Error("이름을 입력해주세요.");
+    this.nickname = value;
+  }
+
+  set Kind(value) {
+    if(value.length === 0) throw new Error("종류를 입력해주세요.");
+    this.kind = value;
+  }
+
+  set Trait(value) {
+    if(value.length === 0) throw new Error("특징을 입력해주세요.");
+    this.trait = value;
+  }
+}
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  const nickname = e.target.nickname.value;
+  const kind = e.target.kind.value;
+  const trait = e.target.trait.value;
+
+  try {
+    // views 가져오기
+    const viewsObj = JSON.parse(localStorage.getItem('views'));
+
+    // 객체 추가
+    const index = viewsObj.length;
+    const instance = new View(index, nickname, kind, trait);
+    viewsObj.push(instance);
+
+    // views 저장
+    const viewsStr = JSON.stringify(viewsObj);
+    localStorage.setItem('views', viewsStr);
+    location.href = '/page/view-profile.html?index=' + index;
+  } catch (e) {
+    // 예외 발생시 메시지 출력
+    alert(e.message);
+    console.error(e);
+  }
+};
+
+newForm.addEventListener("submit", submitHandler);
+
